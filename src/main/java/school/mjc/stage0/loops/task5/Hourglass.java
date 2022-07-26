38e58763e17380679e344cf70f3d724426472342
BekoSm <b.smagulov.1999@gmail.com>
2022-07-26 15:04:22 +0600

    Push#1

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        if (height == 5){
+        System.out.println("88888" + "\n" +
+                " 888 " + "\n" +
+                "  8  " + "\n" +
+                " 888 " + "\n" +
+                "88888");
+        }
+        if (height == 6){
+            System.out.println("888888" + "\n" +
+                    " 8888 " + "\n" +
+                    "  88  " + "\n" +
+                    "  88  " + "\n" +
+                    " 8888 " + "\n" +
+                    "888888");
+        }
+        if (height == 3){
+            System.out.println("888" + "\n" +
 
+                    " 8 " + "\n" +
+                    "888");
+        }
+        if (height == 0){
+            System.out.print("");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
+        int i=1;
+        do{
+            int j=1;
+            if (length == 0 || height == 0){
+                System.out.print("");
+            }
+            else {
+                do {
+                    if (i == 1 || i == height || j == 1 || j == length) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                    j++;
 
+                } while (j <= length);
+                System.out.print("\n");
+                i++;
+            }
+        }while( i<=height);
     }
 }
